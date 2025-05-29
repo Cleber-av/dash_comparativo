dad0daff04f2265169cafcead2b4ebd2a711c340
Cleber-av <observatoriosemusp@gmail.com>
2025-05-29 13:44:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ total_2025 = df_2025[df_2025['TIPO FINAL'] == tipo_escolhido].shape[0]
 filtrado_2025 = df_filtrado.shape[0]
 total_atual = df_filtrado.shape[0]
 total_anterior = df_ano_ant.shape[0]
-variacao = ((total_2025 - total_anterior) / total_anterior * 100) if total_anterior > 0 else 0
+variacao = ((filtrado_2025 - total_anterior) / total_anterior * 100) if total_anterior > 0 else 0
 
 # Total por tipo final apenas para ano 2024
 df_2024 = df[(df['DATA'].dt.year == 2024)]
